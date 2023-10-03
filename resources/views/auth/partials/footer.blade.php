2453e6ea8d6dddd6960e6bb76c245c71636b211c
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-10-03 11:06:55 +0800

    js enable

--- resources/views/auth/partials/footer.blade.php
+++ resources/views/auth/partials/footer.blade.php
@@ -1,4 +1,4 @@
 @livewireScripts
-{{-- <script src="{{ asset('build/assets/app-c8ff57e5.js') }}"></script> --}}
+<script src="{{ asset('build/assets/app-c8ff57e5.js') }}"></script>
 </body>
 </html>
