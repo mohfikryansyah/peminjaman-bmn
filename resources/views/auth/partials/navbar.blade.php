b6e4c82104d0ef61e8d5fa0348bfa9746b85a9b4
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-10-20 13:33:53 +0800

    ...

--- resources/views/auth/partials/navbar.blade.php
+++ resources/views/auth/partials/navbar.blade.php
@@ -43,9 +43,9 @@
                         </div>
                         <ul class="py-1" role="none">
                             <li>
-                                <a href="/dashboard"
+                                <a href="/profile"
                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    role="menuitem">Dashboard</a>
+                                    role="menuitem">Pengaturan</a>
                             </li>
                             <li>
                                 <form method="POST" action="{{ route('logout') }}">
