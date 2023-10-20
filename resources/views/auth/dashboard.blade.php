4e51d31c8c267812537ef9a28b519be919266126
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-10-20 13:34:22 +0800

    jenis barang poli

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -8,7 +8,7 @@
                     class="block cursor-default w-full lg:max-w-xs p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $totalBarang->count() }}
                     </h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">Total barang</p>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">Total jenis barang</p>
                 </a>
                 <a
                     class="block cursor-default w-full lg:max-w-xs p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
