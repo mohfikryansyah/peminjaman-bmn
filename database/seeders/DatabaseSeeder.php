cea1e745c54d0a34136249d9a9d156a6ee256b24
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-10-30 13:49:55 +0800

    fix barang

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,12 +23,12 @@ class DatabaseSeeder extends Seeder
             'name' => 'Administrator',
             'email' => 'admin@bpkhtl15gorontalo.com',
             'password' => 'bpkhtl!23',
-            'verified' => true
+            'verified' => true,
         ]);
         \App\Models\User::factory()->create([
             'name' => 'Fiqriansyah',
             'email' => 'moh.fikryansyah@gmail.com',
-            'password' => 'fiqriansyah2001'
+            'password' => 'fiqriansyah2001',
         ]);
 
         Kasie::create([
@@ -46,7 +46,7 @@ class DatabaseSeeder extends Seeder
             'nipKasie' => 197910041998032001,
             'seksi' => 'Tata Usaha',
         ]);
-        
+
         Role::create(['name' => 'ADMIN']);
         Role::create(['name' => 'PEGAWAI']);
 
@@ -54,301 +54,314 @@ class DatabaseSeeder extends Seeder
         $user = User::find(2);
         $admin->assignRole('ADMIN');
         $user->assignRole('PEGAWAI');
-        
+
+        Barang::create([
+            'nama' => 'Sepeda Motor',
+            'stok' => 5,
+            'kode_barang' => 3020104001,
+            'satuan' => 'Unit',
+        ]);
+        Barang::create([
+            'nama' => 'Battery Charge',
+            'stok' => 4,
+            'kode_barang' => 3030103001,
+            'satuan' => 'Buah',
+        ]);
+
         Barang::create([
             'nama' => 'Rol Meter',
             'stok' => 9,
             'kode_barang' => 3030211003,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
 
         Barang::create([
             'nama' => 'Termometer Standar',
             'stok' => 9,
             'kode_barang' => 3030308012,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Alat Pengukur Garis Tengah',
             'stok' => 9,
             'kode_barang' => 3030309004,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Diameter Tape',
             'stok' => 9,
             'kode_barang' => 3030309006,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Alat Ukur Lainnya',
             'stok' => 14,
             'kode_barang' => 3030309999,
-            'satuan' => 'Dummy'
+            'satuan' => 'Dummy',
         ]);
         Barang::create([
             'nama' => 'Mesin Ketik Elektronik/Selektrik',
             'stok' => 8,
             'kode_barang' => 3050101008,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Mesin Ketik Lainnya',
             'stok' => 1,
             'kode_barang' => 3050101999,
-            'satuan' => 'dummy'
+            'satuan' => 'dummy',
         ]);
         Barang::create([
             'nama' => 'White Board',
             'stok' => 2,
             'kode_barang' => 3050105010,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Peta',
             'stok' => 1,
             'kode_barang' => 3050105014,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'LCD Projector/Infocus',
-            'stok' => 5,
+            'stok' => 6,
             'kode_barang' => 3050105048,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Alat Kantor Lainnya',
             'stok' => 1,
             'kode_barang' => 3050199999,
-            'satuan' => 'Dummy'
+            'satuan' => 'Dummy',
         ]);
         Barang::create([
             'nama' => 'Mesin Penghisap Debu/Vacuum Cleaner',
             'stok' => 1,
             'kode_barang' => 3050203001,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Mesin Pemotong Rumput',
             'stok' => 1,
             'kode_barang' => 3050203003,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Air Cleaner',
             'stok' => 9,
             'kode_barang' => 3050203005,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Sound System',
             'stok' => 2,
             'kode_barang' => 3050206008,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Wireless',
             'stok' => 2,
             'kode_barang' => 3050206012,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Microphone',
             'stok' => 1,
             'kode_barang' => 3050206014,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Dispenser',
             'stok' => 6,
             'kode_barang' => 3050206036,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Digital Audio Storage System',
             'stok' => 2,
             'kode_barang' => 3060101075,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Layar Film/Projector',
             'stok' => 1,
             'kode_barang' => 3060102107,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Camera Digital',
             'stok' => 12,
             'kode_barang' => 3060102128,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Theodolite (Peralatan Studio Pemetaan/peralatan Ukur Tanah)',
             'stok' => 1,
             'kode_barang' => 3060105017,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Kompas Geologi',
             'stok' => 3,
             'kode_barang' => 3060105023,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Clinometer',
             'stok' => 14,
             'kode_barang' => 3060105024,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Kompas (Peralatan Studio Pemetaan/peralatan Ukur Tanah)',
             'stok' => 22,
             'kode_barang' => 3060105035,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'GPS Receiver',
             'stok' => 19,
             'kode_barang' => 3060105038,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Kamera Udara',
             'stok' => 2,
             'kode_barang' => 3060105047,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Handy Talky (HT)',
             'stok' => 10,
             'kode_barang' => 3060201006,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Solar Cell',
             'stok' => 2,
             'kode_barang' => 3060347003,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Tiang Keseimbangan',
             'stok' => 6,
             'kode_barang' => 3070112007,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Digital Signal Procesor',
             'stok' => 1,
             'kode_barang' => 3080111147,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Mesin Power Station White Emergency Diesel Generating',
             'stok' => 1,
             'kode_barang' => 3080140010,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Sprayer',
             'stok' => 2,
             'kode_barang' => 3080141248,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Spiegel Relascope',
             'stok' => 14,
             'kode_barang' => 3080146014,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Wash Bak',
             'stok' => 1,
             'kode_barang' => 3080151024,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Antenna Relay',
             'stok' => 6,
             'kode_barang' => 3080158021,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Serial Scanner/Printer',
             'stok' => 2,
             'kode_barang' => 3080302039,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Refrigerator/Freezer',
             'stok' => 2,
             'kode_barang' => 3080605036,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Kipas Penggerak Air',
             'stok' => 1,
             'kode_barang' => 3090201087,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'GPS',
             'stok' => 9,
             'kode_barang' => 3090403004,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Layar Proyektor',
             'stok' => 1,
             'kode_barang' => 3090407022,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Laptop',
             'stok' => 1,
             'kode_barang' => 3100102002,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Note Book',
             'stok' => 23,
             'kode_barang' => 3100102003,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Tablet PC',
             'stok' => 1,
             'kode_barang' => 3100102009,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Hard Disk',
             'stok' => 1,
             'kode_barang' => 3100201012,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Keyboard (Peralatan Mainframe)',
             'stok' => 1,
             'kode_barang' => 3100201013,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Printer (Peralatan Personal Komputer)',
             'stok' => 41,
             'kode_barang' => 3100203003,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'External/ Portable Hardisk',
             'stok' => 5,
             'kode_barang' => 3100203017,
-            'satuan' => 'Buah'
+            'satuan' => 'Buah',
         ]);
         Barang::create([
             'nama' => 'Baju Pengaman Lainnya',
             'stok' => 2,
             'kode_barang' => 3150201999,
-            'satuan' => 'Dummy'
+            'satuan' => 'Dummy',
         ]);
         // Barang::create([
         //     'nama' => 'Alat Pelindung Lainnya',
